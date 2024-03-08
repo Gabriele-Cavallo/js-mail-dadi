251252698c986e8a9670d6e70672e993f0c6bd05
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-08 14:47:21 +0100

    feat: add array + cicle for

--- js/script.js
+++ js/script.js
@@ -30,5 +30,14 @@ console.log('the-winner-is' , theWinnerIs);
 // 1 - Chiedo all'utente la sua mail
 let userEmail = prompt('Ciao, dimmi la tua mail!!! Scrivi "nome.gmail.com" e premi invio.');
 // 2 - Scorro la lista delle email per accertarmi o meno della presenza della email dell'utente
+const confirmedEmail = ['gianni@gmail.com', 'ermenegildo@gmail.com', 'alberto@gmail.com', 'gabriele@gmail.com', 'matteo@gmail.com', 'sebastiano@gmail.com', 'mario@gmail.com', 'luca@gmail.com', 'franco@gmail.com', 'pippo@gmail.com'];
+let foundEmail = false;
+for (let i = 0; i < confirmedEmail.length; i++) {
+    let thisEmail = confirmedEmail[i];
+    if (userEmail === thisEmail){
+        foundEmail = true;
+    }
+}
 // 3 - Se la mail è presente nella lista stampo il messaggio : Puoi accedere al servizio
 //        - altrimenti stampo il messaggio : Non puoi accedere al servizio
+
