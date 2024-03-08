0d25aa300fc52590fca7e594ebd26d42be329ca6
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-08 13:47:12 +0100

    feat: add task 1 js structure

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// 1 - Genero un numero random per il giocatore da 1 a 6
+// 2 - Genero un numero random per il computer da 1 a 6
+// 3 - Confronto i due numeri
+// 4 - Dichiaro vincitore chi ha il numero più alto
+
 // TASK 2 - Mail
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
