e7bd72485a32729c2ce616bd8b7e02ffcfd93ac3
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-08 14:56:09 +0100

    fix: number generators

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // 1 - Genero un numero random per il giocatore da 1 a 6
-let playerNumber = Math.floor(Math.random() * 7);
+let playerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('player-number' , playerNumber);
 // 2 - Genero un numero random per il computer da 1 a 6
-let computerNumber = Math.floor(Math.random() * 7);
+let computerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('computer-number' , computerNumber);
 // 3 - Confronto i due numeri
 //       - Dichiaro vincitore chi ha il numero più alto
