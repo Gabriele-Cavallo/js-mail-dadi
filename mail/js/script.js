d9daf555689eb848212e702babbd41b67c50be6a
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-08 15:58:44 +0100

    feat: add dice folder + mail folder

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,26 +1,3 @@
-// TASK 1 - Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-// 1 - Genero un numero random per il giocatore da 1 a 6
-let playerNumber = Math.floor(Math.random() * 6) + 1;
-console.log('player-number' , playerNumber);
-// 2 - Genero un numero random per il computer da 1 a 6
-let computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log('computer-number' , computerNumber);
-// 3 - Confronto i due numeri
-//       - Dichiaro vincitore chi ha il numero più alto
-let winner = 'Il vincitore è il ';
-let theWinnerIs;
-if (playerNumber > computerNumber){
-    theWinnerIs = `${winner}giocatore!!!`;
-}else if(computerNumber > playerNumber){
-    theWinnerIs = `${winner}computer!!!`;
-}else if (computerNumber === playerNumber){
-    theWinnerIs = `Pari!!!Nessuno vince!!!`;
-}
-console.log('the-winner-is' , theWinnerIs);
-
 // TASK 2 - Mail
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
